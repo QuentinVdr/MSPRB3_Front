21a04b5eba1f27d548e81faba631c8f29904a832
Quentin Verdier <quent1.verdier@gmail.com>
2024-01-29 18:43:53 +0100

    🚀 Add react-leaflet-markercluster package and display markers on the map
    The code changes include adding the "react-leaflet-markercluster" package to the project's dependencies in the package.json file. This package allows for clustering of markers on the map.
    
    In the Home.jsx file, two new plant objects are added to the "plantes" array with their respective latitude and longitude coordinates. These objects represent markers that will be displayed on the map.
    
    Additionally, a Grid item is modified to adjust the layout of components on the page. The MapContainer component now renders multiple Marker components based on the "plantes" array, each displaying a popup with the name of the plant.
    
    ℹ️ Note: The MarkerClusterGroup component is commented out for now.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,20 +7,21 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import styles from './Home.module.scss';
 
 /**
- * Page that contains all the components displayed on the application homepage
+* Page that contains all the components displayed on the application homepage
+
  */
 export default function Home() {
   const mapRef = useRef();
   const center = L.latLng(47.216671, -1.55);
-
   const plantes = [
-    { id: 1, name: 'omg plante' },
-    { id: 2, name: 'plante ?' }
+    { id: 1, name: 'omg plante', latitude: 47.216671, longitude: -1.56 },
+
+    { id: 2, name: 'plante ?', latitude: 47.216671, longitude: -1.55 }
   ];
 
   return (
     <Grid container direction="row" className={styles.homePage}>
-      <Grid item xs={3} container direction="column" gap={3} className={styles.plantesList}>
+      <Grid item xs={6} sm={5} md={4} lg={3} container direction="column" gap={3} className={styles.plantesList}>
         <Stack direction="row" justifyContent="space-between">
           <Typography variant="h2">Les plantes</Typography>
           <Stack alignItems="center" justifyContent="center">
@@ -40,17 +41,19 @@ export default function Home() {
           </Stack>
         </Stack>
       </Grid>
-      <Grid item xs={9}>
+      <Grid item xs={6} sm={7} md={8} lg={9}>
         <MapContainer className={styles.mapContainer} center={center} zoom={13} scrollWheelZoom={false} ref={mapRef}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={center}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          {/* <MarkerClusterGroup maxClusterRadius={50}> */}
+          {plantes.map((plante) => (
+            <Marker position={L.latLng(plante.latitude, plante.longitude)} key={plante.id}>
+              <Popup>{plante.name}</Popup>
+            </Marker>
+          ))}
+          {/* </MarkerClusterGroup> */}
         </MapContainer>
       </Grid>
     </Grid>
