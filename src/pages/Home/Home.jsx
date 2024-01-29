b0bc2525650fe43db515d317211e0d0a39cf8073
Quentin Verdier <quent1.verdier@gmail.com>
2024-01-29 17:23:16 +0100

    🎉 Added plant list and filter button to the home page
    - Imported FilterAltIcon from @mui/icons-material
    - Added Button, Card, and Stack components from @mui/material
    - Created a plant list with sample data
    - Styled the plant list container and map container in Home.module.scss

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
-import { Grid, Typography } from '@mui/material';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import { Button, Card, Grid, Stack, Typography } from '@mui/material';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useRef } from 'react';
@@ -12,13 +13,35 @@ export default function Home() {
   const mapRef = useRef();
   const center = L.latLng(47.216671, -1.55);
 
+  const plantes = [
+    { id: 1, name: 'omg plante' },
+    { id: 2, name: 'plante ?' }
+  ];
+
   return (
-    <Grid container direction="row" className={styles.home_page}>
-      <Grid item xs={2}>
-        <Typography variant="h2">Bienvenue</Typography>
+    <Grid container direction="row" className={styles.homePage}>
+      <Grid item xs={3} container direction="column" gap={3} className={styles.plantesList}>
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant="h2">Les plantes</Typography>
+          <Stack alignItems="center" justifyContent="center">
+            <Button variant="outlined" startIcon={<FilterAltIcon />}>
+              Filtres
+            </Button>
+          </Stack>
+        </Stack>
+        <Stack direction="column" gap={2}>
+          <Typography variant="h3">plante</Typography>
+          <Stack direction="column" gap={1}>
+            {plantes.map((plante) => (
+              <Card variant="outlined" key={plante.key}>
+                <Typography variant="body1">{plante.name}</Typography>
+              </Card>
+            ))}
+          </Stack>
+        </Stack>
       </Grid>
-      <Grid item xs={10}>
-        <MapContainer className={styles.map_container} center={center} zoom={13} scrollWheelZoom={false} ref={mapRef}>
+      <Grid item xs={9}>
+        <MapContainer className={styles.mapContainer} center={center} zoom={13} scrollWheelZoom={false} ref={mapRef}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
