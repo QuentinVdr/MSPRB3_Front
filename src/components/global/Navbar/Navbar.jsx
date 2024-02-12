1cca0c6ce1633021260b30353e1555e3c42f3dbd
Quentin VERDIER <quent1.verdier@gmail.com>
2024-02-12 19:47:32 +0100

    🐛 Refactor Navbar component and remove unnecessary code

--- src/components/global/Navbar/Navbar.jsx
+++ src/components/global/Navbar/Navbar.jsx
@@ -1,12 +1,10 @@
-import { AppBar, Container, Typography } from '@mui/material';
+import { AppBar, Typography } from '@mui/material';
 import styles from './Navbar.module.scss';
 
 export default function Navbar() {
   return (
-    <AppBar className={styles.navbar}>
-      <Container maxWidth="xl">
-        <Typography variant="h1">Navbar</Typography>
-      </Container>
+    <AppBar className={styles.navbar} position="sticky">
+      <Typography variant="h1">Navbar</Typography>
     </AppBar>
   );
 }
