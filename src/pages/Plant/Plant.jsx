e3295b061e10d3ce481246db4b1bd2ad755f78d7
Quentin VERDIER <quent1.verdier@gmail.com>
2024-02-16 11:17:30 +0100

    🚧 WIP my plant page

--- src/pages/Plant/Plant.jsx
+++ src/pages/Plant/Plant.jsx
@@ -1,15 +1,53 @@
-import { Stack, Typography } from '@mui/material';
+import { Button, Grid, Stack, Typography } from '@mui/material';
+import { plants } from '@pages/Home/Home';
+import { useState } from 'react';
 import styles from './Plant.module.scss';
 
 export default function Plant() {
+  const [selectedPlant, setSelectedPlant] = useState(plants[0]);
+
+  const addNewPlant = () => {
+    console.log('add new plant');
+  };
+
   return (
-    <Stack direction="row" className={styles.plantPage}>
-      <Stack direction="column">
-        <Typography variant="body1">Mes plantes</Typography>
-      </Stack>
-      <Stack direction="column">
-        <Typography variant="body1">Ajouter une plante</Typography>
-      </Stack>
-    </Stack>
+    <Grid container direction="row" spacing={3} className={styles.plantPage}>
+      <Grid item md={3} container direction="column" gap={2}>
+        <Typography variant="h2">Mes plantes</Typography>
+        <Stack direction="column" gap={1}>
+          {plants.map((plant, index) => (
+            <div
+              key={`${plant.key} ${plant.name}`}
+              onClick={() => setSelectedPlant(plant)}
+              className={styles.myPlantsCard}
+              role="button"
+              tabIndex={index}
+            >
+              <Typography variant="body1">{plant.name}</Typography>
+            </div>
+          ))}
+          <Button variant="contained" onClick={addNewPlant}>
+            Ajouter une nouvelle place
+          </Button>
+        </Stack>
+      </Grid>
+      <Grid item md={9} container direction="column" gap={2}>
+        <Typography variant="h2">Plantes detail</Typography>
+        <Stack direction="column" gap={2} className={styles.selectedPlantCard}>
+          <Typography variant="h2">{selectedPlant.name}</Typography>
+          <Grid container direction="row" spacing={1}>
+            <Grid item xs={12}>
+              <Typography variant="body1">{selectedPlant.description}</Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body1">{selectedPlant.address}</Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body1">{selectedPlant.city}</Typography>
+            </Grid>
+          </Grid>
+        </Stack>
+      </Grid>
+    </Grid>
   );
 }
