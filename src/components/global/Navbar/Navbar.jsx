5bbad1737e7f8584800405614501d619c7cce412
Quentin Verdier <quent1.verdier@gmail.com>
2024-02-21 16:17:34 +0100

    🐛 Add link to Articles in Navbar

--- src/components/global/Navbar/Navbar.jsx
+++ src/components/global/Navbar/Navbar.jsx
@@ -39,10 +39,10 @@ export default function Navbar() {
           <Link to="/my-plant">
             <Typography variant="body1">Mes Plantes</Typography>
           </Link>
-            <Link to="/articles">
-              <Typography variant="body1">Articles</Typography>
-            </Link>
         )}
+        <Link to="/articles">
+          <Typography variant="body1">Articles</Typography>
+        </Link>
         <div className={styles.userMenu}>
           <IconButton
             size="large"
