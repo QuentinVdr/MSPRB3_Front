712c7e4c49d1f399b89966de34516955b2965944
Quentin Verdier <quent1.verdier@gmail.com>
2024-02-28 10:52:30 +0100

    ✨ Refactor plant detail and plants list sections

--- src/pages/MyPlants/MyPlants.jsx
+++ src/pages/MyPlants/MyPlants.jsx
@@ -36,44 +36,8 @@ export default function MyPlant() {
 
   return (
     <>
-      <Grid
-        container
-        direction={{ xs: 'column-reverse', md: 'row' }}
-        spacing={3}
-        wrap="nowrap"
-        className={styles.plantsPage}
-      >
-        <Grid item xs md={3}>
-          <Stack direction="column" gap={2} className={styles.plantsListPanel}>
-            <Typography variant="h2">Mes plantes</Typography>
-            <Stack direction="column" gap={1} className={styles.plantsList}>
-              {plants.map((plant) => (
-                <Stack
-                  key={`${plant.id} ${plant.name}`}
-                  onClick={() => setSelectedPlant(plant)}
-                  className={styles.myPlantsCard + (plant == selectedPlant ? ` ${styles.selectedPlantCard}` : '')}
-                  direction="row"
-                  justifyContent="space-between"
-                  alignItems="center"
-                >
-                  <Typography variant="body1">{plant.name}</Typography>
-                  <Stack direction="row">
-                    <IconButton onClick={() => handleUpdate(plant)} color="primary">
-                      <EditIcon />
-                    </IconButton>
-                    <IconButton onClick={() => handleDelete(plant)}>
-                      <DeleteIcon />
-                    </IconButton>
-                  </Stack>
-                </Stack>
-              ))}
-            </Stack>
-            <Button variant="contained" onClick={handleOpen} className={styles.addPlantButton}>
-              Ajouter une nouvelle plante
-            </Button>
-          </Stack>
-        </Grid>
-        <Grid item xs md={9} container direction="column" gap={2}>
+      <Stack direction={{ xs: 'column', md: 'row-reverse' }} spacing={3} className={styles.plantsPage}>
+        <Stack direction="column" gap={2} className={styles.plantDetailSection}>
           <Typography variant="h2">Plante detail</Typography>
           <Stack direction="column" gap={2} className={styles.selectedPlantDetail}>
             <Typography variant="h2">{selectedPlant.name}</Typography>
@@ -104,15 +68,43 @@ export default function MyPlant() {
                     : `${selectedPlant.name} n'a pas besoin d'aide`}
                 </Typography>
               </Grid>
+              <Stack direction="row" flexWrap="nowrap" className={styles.selectedPlantImages}>
+                {selectedPlant.images.map((image, index) => (
+                  <img key={image} src={image} alt={`Image ${index}`} className={styles.selectedPlantImage} />
+                ))}
+              </Stack>
             </Grid>
-            <Stack direction="row" flexWrap="nowrap" className={styles.selectedPlantImages}>
-              {selectedPlant.images.map((image, index) => (
-                <img key={image} src={image} alt={`Image ${index}`} className={styles.selectedPlantImage} />
-              ))}
-            </Stack>
           </Stack>
-        </Grid>
-      </Grid>
+        </Stack>
+        <Stack direction="column" gap={2} className={styles.plantsListSection}>
+          <Typography variant="h2">Mes plantes</Typography>
+          <Stack direction="column" gap={1} className={styles.plantsList}>
+            {plants.map((plant) => (
+              <Stack
+                key={`${plant.id} ${plant.name}`}
+                onClick={() => setSelectedPlant(plant)}
+                className={styles.myPlantsCard + (plant == selectedPlant ? ` ${styles.selectedPlantCard}` : '')}
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Typography variant="body1">{plant.name}</Typography>
+                <Stack direction="row">
+                  <IconButton onClick={() => handleUpdate(plant)} color="primary">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton onClick={() => handleDelete(plant)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </Stack>
+              </Stack>
+            ))}
+          </Stack>
+          <Button variant="contained" onClick={handleOpen} className={styles.addPlantButton}>
+            Ajouter une nouvelle plante
+          </Button>
+        </Stack>
+      </Stack>
       <Modal open={isPlantFormOpen} onClose={handleClose} className={styles.plantFormModal}>
         <Stack gap={3} className={styles.plantFormCard}>
           <PlantForm afterValidation={handleClose} afterCancel={handleClose} defaultPlant={selectedPlantUpdate} />
