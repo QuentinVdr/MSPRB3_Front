bc15ffa71e161a76a00d50fde43ab4ce76d3417a
Quentin VERDIER <quent1.verdier@gmail.com>
2024-03-07 17:06:39 +0100

    🔧 Refactor login functionality & add author to articles/plants
    - Updated login logic to iterate through users array
    - Changed email field to mail in SignInForm component
    - Added author property to articles and plants data
    - Included new user objects in User data store

---
+++ src/stores/dataStore/User.js
@@ -0,0 +1,37 @@
+export const botanistUser = {
+  id: 1,
+  firstName: 'user',
+  lastName: 'botanist',
+  mail: 'botanist@mail.com',
+  password: 'botanist',
+  isBotanist: true
+};
+
+export const botanistUser2 = {
+  id: 2,
+  firstName: 'user2',
+  lastName: 'botanist',
+  mail: 'botanist2@mail.com',
+  password: 'botanist',
+  isBotanist: true
+};
+
+export const casualUser = {
+  id: 10,
+  firstName: 'user',
+  lastName: 'casual',
+  mail: 'user@mail.com',
+  password: 'user',
+  isBotanist: false
+};
+
+export const casualUser2 = {
+  id: 11,
+  firstName: 'userZ',
+  lastName: 'casual',
+  mail: 'userZ@mail.com',
+  password: 'user',
+  isBotanist: false
+};
+
+export const users = [botanistUser, casualUser];
