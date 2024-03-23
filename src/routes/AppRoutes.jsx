d92d784d56cfa53b9542a430d1a4a898b1c24408
Quentin Verdier <quent1.verdier@gmail.com>
2024-03-23 19:10:48 +0100

    ♻️ Refactor router and add AppProviders component

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,28 +4,31 @@ import CGU from '@pages/CGU/CGU';
 import Home from '@pages/Home/Home';
 import MyPlants from '@pages/MyPlants/MyPlants';
 import { NotFound } from '@pages/NotFound/NotFound';
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import AuthRoute from './accessHandler/AuthRoute';
 import Layout from './components/Layout';
 
 /**
- * Variable used to handle all the routes of the application
+ * Componant to handle all the routes of the application
  */
-export const router = () =>
-  createBrowserRouter(
-    createRoutesFromElements(
-      <Route element={<Layout />}>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/articles" element={<Articles />} />
-        <Route path="/cgu" element={<CGU />} />
-        {/** page required authentification */}
-        <Route element={<AuthRoute />}>
-          <Route path="/my-plants" element={<MyPlants />} />
+export const AppRouter = () => (
+  <RouterProvider
+    router={createBrowserRouter(
+      createRoutesFromElements(
+        <Route element={<Layout />}>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/cgu" element={<CGU />} />
+          {/** page required authentification */}
+          <Route element={<AuthRoute />}>
+            <Route path="/my-plants" element={<MyPlants />} />
+          </Route>
+          <Route path="/not-found" element={<NotFound />} />
+          {/** Unknown path redirection */}
+          <Route path="*" element={<NotFound />} />
         </Route>
-        <Route path="/not-found" element={<NotFound />} />
-        {/** Unknown path redirection */}
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    )
-  );
+      )
+    )}
+  />
+);
