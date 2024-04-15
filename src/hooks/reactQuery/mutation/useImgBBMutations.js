de1650a61c39ba8f1610d6d32dbd082f660a353f
Quentin VERDIER <quent1.verdier@gmail.com>
2024-04-15 22:40:19 +0200

    ✨ Add handle for several images

--- src/hooks/reactQuery/mutation/useImgBBMutations.js
+++ src/hooks/reactQuery/mutation/useImgBBMutations.js
@@ -7,8 +7,27 @@ export const useUploadImageMutation = () => {
 
   return useMutation({
     mutationFn: (image, expiration) => uploadImage(image, expiration),
-    onSuccess: (savedExemple) => {
-      queryClient.setQueryData(exempleQKey.detail(savedExemple.id), savedExemple);
+    onSuccess: (savedImage) => {
+      queryClient.setQueryData(exempleQKey.detail(savedImage.id), savedImage);
+    }
+  });
+};
+
+export const useUploadImagesMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (images, expiration) => {
+      return await Promise.all(
+        images.map(async (image) => {
+          return await uploadImage(image, expiration);
+        })
+      );
+    },
+    onSuccess: (savedImages) => {
+      savedImages.forEach((savedImage) => {
+        queryClient.setQueryData(exempleQKey.detail(savedImage.data.id), savedImage.data);
+      });
     }
   });
 };
