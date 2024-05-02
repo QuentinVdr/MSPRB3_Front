90ea3af7b41b8d57a47967053cb1008443002991
Quentin Verdier <quent1.verdier@gmail.com>
2024-05-02 09:44:16 +0200

    ✨ WIP Add chat

--- src/hooks/reactQuery/queries/useAddressQueries.js
+++ src/hooks/reactQuery/queries/useAddressQueries.js
@@ -5,6 +5,8 @@ import { useQuery } from '@tanstack/react-query';
 export const useAddressDetailQuery = (address, options) =>
   useQuery({
     ...options,
+    cacheTime: 1000 * 60 * 60 * 24,
+    staleTime: 1000 * 60 * 60 * 24,
     queryKey: addressQKey.detail(address),
     queryFn: () => findAddressDetail(address)
   });
